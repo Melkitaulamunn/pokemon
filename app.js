75b92c17b7807c7712f49399e9bef9dfaf397791
Ayşe ALBAYRAK <aysche17@gmail.com>
2023-01-09 15:57:54 +0300

    first episode

---
+++ app.js
@@ -0,0 +1,17 @@
+const bg_color = {
+    grass: '#8BD369',
+    fire: '#FF603F',
+    water: '#3399FF',
+    bug: '#AABB22',
+    normal: '#AAAA99',
+    flying: '#9AA8FA',
+    poison: '#B76EA4',
+    electric: '#FFD34E',
+    ground: '#E2C56A',
+    fairy: '#F1A8EC',
+    psychic: '#FF6EA4',
+    fighting: '#C56E5C',
+    rock: '#C5B679',
+    dragon: '#7766EE',
+    ice: '#66CCFF',
+  }
